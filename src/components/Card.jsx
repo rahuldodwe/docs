a47440c2778e879d3d57c52a89bea46eadf188ef
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-08-06 14:52:51 +0530

    Project Completeted

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import { FaRegFileAlt } from "react-icons/fa";
 import { BsDownload } from "react-icons/bs";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion"
 
 
 
-function Card() {
+
+function Card({ data, reference }) {
   return (
-    <div>
-      <div className='relative w-60 h-72 rounded-[50px] bg-zinc-900/50 text-white px-8 py-10 overflow-hidden'>
+    <>
+      <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.2}} className='relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/50 text-white px-8 py-10 overflow-hidden'>
         <FaRegFileAlt />
-        <p className='text-sm leading-tight mt-5 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-        <div className="card-footer absolute bottom-0 w-full px-8 left-0">
-          <div className='flex items-center justify-between py-3  mb-5'>
-            <h1>.4mb</h1>
+        <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
+        <div className="card-footer absolute bottom-0 w-full left-0">
+          <div className='flex items-center justify-between px-8 py-3  mb-3'>
+            <h1>{data.filesize}</h1>
             <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
-              <BsDownload size=".7em" color="#fff" />
+              {data.close ? <IoClose /> : <BsDownload size=".7em" color="#fff" />}
             </span>
           </div>
+          {
+            data.cardFooter.isOpen ? (
+              <div className={`tag w-full py-4 ${data.cardFooter.footerColor === "blue" ? "bg-blue-600" : "bg-green-600"} flex items-center justify-center`}>
+                <h1 className='text-sm font-semibold'>{data.cardFooter.footerTitle}</h1>
+              </div>
+            ) : null
+          }
         </div>
-      </div>
-    </div>
+      </motion.div>
+      </>
   )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
 
 function Foreground() {
+
+  const ref = useRef(null);
+
+  const data = [
+    {
+      desc: "This is the background color of the card adipisicing.",
+      filesize: "4.9mb",
+      close: true,
+      cardFooter: { isOpen: false, footerTitle: "Download Now", footerColor: "green"},
+    },
+    {
+      desc: "This is the background color of the card adipisicing.",
+      filesize: "4.9mb",
+      close: false,
+      cardFooter: { isOpen: true, footerTitle: "Download Now", footerColor: "blue"},
+    },
+    {
+      desc: "This is the background color of the card adipisicing.",
+      filesize: "4.9mb",
+      close: true,
+      cardFooter: { isOpen: true, footerTitle: "Upload", footerColor: "green"},
+    },
+
+  ];
   return (
     <div>
-      <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-        <Card></Card>
+      <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+        {data.map((item, index)=>(
+          <Card data={item} reference={ref} />
+        ))}
       </div>
     </div>
   )
